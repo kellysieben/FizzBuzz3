5b691cf43dc1fd108090584da719160faf85144d
Sieben Kelly(Consultant) <KSieben@ipf.lan>
2016-12-05 16:34:42 -0700

    First commit

---
+++ src/FizzBuzzer.py
@@ -0,0 +1,3 @@
+def FBGo(n):
+    return ("Fizz"*(n%3==0) + "Buzz"*(n%5==0) or str(n))
+# ---eofunc---
---
+++ test/test_FizzBuzzer.py
@@ -0,0 +1,22 @@
+import pytest
+from FizzBuzzer import FBGo
+
+@pytest.mark.parametrize("x,y", [
+    (4, "4"),
+    (0, "FizzBuzz"),
+    (-1, "-1"),
+    (3, "Fizz"),
+    (5, "Buzz"),
+    (15, "FizzBuzz"),
+    (21, "Fizz"),
+    (30, "FizzBuzz"),
+    (45, "FizzBuzz"),
+    (25, "Buzz"),
+    (98, "98"),
+    (99, "Fizz"),
+])
+
+def test_RunAll(x, y):
+    assert FBGo(x) == y
+    
+#eof
